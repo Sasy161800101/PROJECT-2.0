e398e291f3d20ffb3aa421039646a7db582b4b8c
Federico1620 <federicocirella1997@gmail.com>
2025-10-16 12:03:31 +0200

    modifiche

--- project2/src/pages/prodotto.tsx
+++ project2/src/pages/prodotto.tsx
@@ -74,7 +74,7 @@ function Prodotto () {
         value="1"
         className="h-10 w-16 rounded-sm border-gray-200 text-center [-moz-appearance:_textfield] sm:text-sm [&::-webkit-inner-spin-button]:m-0 [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:m-0 [&::-webkit-outer-spin-button]:appearance-none"
       />
-      <button type="button" className="size-10 leading-10 text-gray-600 transition hover:opacity-75">
+      <button onClick={addToCart} type="button" className="size-10 leading-10 text-gray-600 transition hover:opacity-75">
         +
       </button>
       <select
